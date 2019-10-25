d0b64582214408e36936b8745f0aacceaa27c355
philippegatti <ph.gatti@gmail.com>
2019-10-25 10:31:37 +0200

    liens dogs corriges

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,4 @@
 class Dog < ApplicationRecord
+	has_many :join_dog_strolls
 	has_many :strolls, through: :join_dog_stroll
 end
--- app/models/stroll.rb
+++ app/models/stroll.rb
@@ -1,4 +1,5 @@
 class Stroll < ApplicationRecord
+	has_many :join_dog_strolls
 	has_many :dogs, through: :join_dog_stroll
   	belongs_to :dogsitter
 end
